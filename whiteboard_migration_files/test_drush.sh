6aa80d5b150edb7cf882b4c0d8e922db3e7d9e3d
piyuesh23 <piyuesh23@qed42.com>
2014-12-08 11:40:44 +0530

    Whiteboard files migration

---
+++ whiteboard_migration_files/test_drush.sh
@@ -0,0 +1,32 @@
+echo "Moves the completed job related files to s3 & saves the s3 url.";
+drush mwef
+echo "\n";
+echo "Moves the completed job's extra files sent to client to s3 & saves the s3 url."
+drush mwefsc
+echo "\n";
+echo "Moves the completed job related files sent to client to s3 & saves the s3 url."
+drush mwfsc
+echo "\n";
+echo "Moves the completed job's feedback files to s3 & saves the s3 url.";
+drush mwjff
+echo "\n";
+echo "Moves the completed job's question extra files to s3 & saves the s3 url.";
+drush mwjqef
+echo "\n";
+echo "Moves the completed job's question files to s3 & saves the s3 url.";
+drush mwjqf
+echo "\n";
+echo "Moves the completed job questions's reference files to s3 & saves the s3 url.";
+drush mwjqrf
+echo "\n";
+echo "Moves the completed job question's sent files to s3 & saves the s3 url.";
+drush mwjqsf
+echo "\n";
+echo "Moves the completed job question's uploaded files to s3 & saves the s3 url.";
+drush mwjquf
+echo "\n";
+echo "Moves the completed job related processed files to s3 & saves the s3 url.";
+drush mwpf
+echo "\n";
+echo "Moves the completed job related reference files to s3 & saves the s3 url.";
+drush mwrf
